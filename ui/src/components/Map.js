9175383a9b5fd2fe91e77f4b349c04ac36efca4a
jihoonshin <jip68660@gmail.com>
2019-11-07 13:42:37 -0600

    Fix error on WIP

--- ui/src/components/Map.js
+++ ui/src/components/Map.js
@@ -8,30 +8,28 @@ class Map extends React.Component {
 
         this.state={
             center: null, //"user" will be center
-            zoom: 14,
+            zoom: 15,
             markers: [] //{"class", "lat", "lng", "level"}
         }
         this.displayMarkers = this.displayMarkers.bind(this);
     }
     componentDidMount() {
         if (this.props.address !== "") {
-            // console.log(this.props.address);
-            // const fetchPromise =  fetch(`https://basket.hyunchel.dev?address=${ this.props.address }`);
-            // fetchPromise.then(response => {
-            //     return response.json();
-            // }).then((data) => {
-            //     const temp = this.state.markers.concat(data.nearestCoordinates); 
-            //     this.setState({
-            //         center: data.targetCoordinate,
-            //         markers: temp
-            //     });
-            //     console.log(this.state.center);
-            //     console.log(this.state.markers);
-            // });
-            this.setState({
-                center: { lat: 32.9876178, long: -96.77303},
-                markers:[{"lat":"32.987857","long":"-96.772993"},{"lat":"32.987962","long":"-96.77311"},{"lat":"32.987962","long":"-96.77311"}]
-            })
+            console.log(this.props.address);
+            const fetchPromise =  fetch(`https://basket.hyunchel.dev?address=${ this.props.address }`);
+            fetchPromise.then(response => {
+                return response.json();
+            }).then((data) => {
+                const temp = this.state.markers.concat(data.nearestCoordinates); 
+                this.setState({
+                    center: { lat: data.targetCoordinate.lat, lng: data.targetCoordinate.long },
+                    markers: temp
+                });
+            });
+            // this.setState({
+            //     center: { lat: 32.9876178, lng: -96.77303},
+            //     markers:[{"lat":"32.987857","lng":"-96.772993"},{"lat":"32.987962","lng":"-96.77311"},{"lat":"32.987962","lng":"-96.77311"}]
+            // })
         }
     }
     displayMarkers = () => {
@@ -45,25 +43,25 @@ class Map extends React.Component {
                 />
             )
         });
-        // console.log(this.props.address);
+        console.log(displayDogs);
         return displayDogs;
     } 
     render() {
+        console.log("After:" + this.state.center);
         const map = this.state.center ? (
             <GoogleMapReact
                 bootstrapURLKeys={{ key: "AIzaSyAyL5y5X8IPLWPVil6qwDah5PAK9C80d-Q" }}
-                defaultCenter={ this.state.center }
+                // defaultCenter={ this.state.center }
+                center={ this.state.center }
                 defaultZoom={ this.state.zoom }
             >
             { this.displayMarkers() }
         
             </GoogleMapReact>
         ):null;
-        console.log(map);
 
         return (
-            <div style={{ margin: 'auto', height: '90vh', width: '90vw' }}>                
-                {/* { this.changeCenter({lat:31.89, lng:-97.04}) } */}
+            <div style={{ margin: 'auto', height: '90vh', width: '90vw' }}>  
             { map }
             </div>
         );
