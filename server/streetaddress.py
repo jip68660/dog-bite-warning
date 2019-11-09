3c1f8dbbcd4a5ea05f9d192a81481e3232033445
Raymond Kim <raymond.gitae.kim@gmail.com>
2019-11-09 16:47:21 -0600

    Add activity, date info

--- server/streetaddress.py
+++ server/streetaddress.py
@@ -1,22 +1,20 @@
 import json
 
-
 def open_file(filename):
     """Read JSON file content as UTF8 encoding."""
     with open(filename, encoding='utf-8') as json_file:
         return json.load(json_file)
 
 
-def get_dog_info():
+def get_dog_coordinates():
     """Return a list of all reported coordinates."""
     data = open_file('incidents.json')
-    dog_info = []
+    dog_coordinates = []
     for i in data['data']:
-        dog_info.append(i)
-    return dog_info
+        print(i)
+        break
+        dog_coordinates.append(i[-6][-4:-2])
+    return dog_coordinates
 
- 
 
-
-
-    
+get_dog_coordinates()
