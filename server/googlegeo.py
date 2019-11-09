ee654b574f7bc853f84060d62261e5c8da5a3c03
Hyunchel Kim <hyunchel.inbox@gmail.com>
2019-11-09 16:02:46 -0600

    Fix linters

--- server/googlegeo.py
+++ server/googlegeo.py
@@ -6,7 +6,7 @@ def locate(address):
     """Return coordicate of given address."""
     g = geocoder.google(address)
     return g.latlng
-    
+
 
 def get_distance(c1, c2):
     """Return distance between two coordinates."""
