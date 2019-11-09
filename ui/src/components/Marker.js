ecb9980ac6797c6a278ea07cf8b1dd33b3fac3e0
jihoonshin <jip68660@gmail.com>
2019-11-09 14:58:18 -0600

    Add modal-bootstrap

--- ui/src/components/Marker.js
+++ ui/src/components/Marker.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal } from 'react-bootstrap';
 import './Marker.css';
 
 const Marker = (props) => {    
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <div 
-            className={`marker ${ props.class }`}
-        >    
-        </div>    
+        <>
+            <button 
+                className={`marker ${ props.class }`}
+                onClick= { handleShow }
+            >    
+            </button> 
+            
+            <Modal 
+                show={ show }
+                onHide={ handleClose }
+                animation={ false }
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Details</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    { props.lat + "...." + props.lng }
+                </Modal.Body>
+                <Modal.Footer>
+                    <button onClick={ handleClose }>
+                    Close
+                    </button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 };
 export default Marker;
